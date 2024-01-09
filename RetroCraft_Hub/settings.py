34cff49f9f72b00ed86f874c8020fd5384462a71
kunalvirwal <kunalvirwal@gmail.com>
2024-01-09 12:53:15 +0530

    Changes for production

--- RetroCraft_Hub/settings.py
+++ RetroCraft_Hub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gjg*hu+bj+o6-qoqxvzgux63-u(t9%5!e0r6)jsvmxf*(hsq(&'
+SECRET_KEY = os.environ.get("SECRET_KEY") #'django-insecure-gjg*hu+bj+o6-qoqxvzgux63-u(t9%5!e0r6)jsvmxf*(hsq(&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower()=='true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
