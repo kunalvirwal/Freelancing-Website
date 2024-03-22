4757fe1bc3c5905a93c1b60e7faa0da7f4382069
kunalvirwal <149074679+kunalvirwal@users.noreply.github.com>
2024-03-22 18:34:50 +0530

    Update settings.py
    Bug fixes

--- RetroCraft_Hub/settings.py
+++ RetroCraft_Hub/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY") #'django-insecure-gjg*hu+bj+o6-qoqxvzgux63-u(t9%5!e0r6)jsvmxf*(hsq(&'
-###SECRET_KEY='django-insecure-gjg*hu+bj+o6-qoqxvzgux63-u(t9%5!e0r6)jsvmxf*(hsq(&'
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower()=='true'
